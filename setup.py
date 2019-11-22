c4f4d4113be0f0951da4d9ed6f790da79cbe4267
gustav-gustav <gustavo.guga.barros@hotmail.com>
2019-11-22 09:09:24 -0300

    added requirements check

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os, glob, io
+import os, glob, io, subprocess
 #run this to setup
 
 path = os.environ['LYRICS_PATH']
@@ -7,3 +7,6 @@ files = glob.glob(os.path.join(path, '*.txt'))
 if full_filename not in files:
     with io.open(full_filename, 'w', encoding='utf-8') as f:
             f.write('dummy text')
+
+requirements = 'spotipy==2.4.4 beautifulsoup4==4.6.3 requests==2.19.1'
+subprocess.call(f'pip install {requirements}', shell=True)
