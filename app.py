789e5fde7873061a3f62bfff717bcbb668e0e86c
gustav-gustav <gustavo.guga.barros@hotmail.com>
2019-11-22 21:46:59 -0300

    functioning flask app that runs alongside lyrics.py

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-import os, time, io
-from flask import Flask, render_template
+import os, time, io, datetime, json
+from flask import Flask, render_template, request, render_template_string
 
 app = Flask(__name__)
 
 @app.route("/")
 def test():
-    while True:
-        with io.open('lyrics.txt', 'r') as f:
-            HEAD = f.readline().strip("\n")
-            BODY = f.read().split('\n')
+    return render_template('test.html')
+    # return render_template("home.html", lyrics=reader())
 
-        lyrics = {"HEAD": HEAD, "BODY": BODY}
-        return render_template("home.html", lyrics=lyrics)
+@app.route('/data')
+def data():
+    return reader()
+
+def reader():
+    with io.open('spotify.json', 'r') as f:
+        js = json.load(f)
+        js_url = js["item"]["album"]["images"][0]["url"]
+
+    with io.open('lyrics.txt', 'r') as f:
+        HEAD = f.readline().strip("\n")
+        BODY = f.read().replace('\n', '<br>')
+
+    lyrics = {"HEAD": HEAD, "BODY": BODY, 'IMAGE': js_url}
+    return lyrics
 
 
 if __name__ == "__main__":
